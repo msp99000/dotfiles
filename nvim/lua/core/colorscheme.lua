5daedd4c772fad2935a4173dedda8668069d4293
Manpreet Singh <msp99000@gmail.com>
2024-06-26 15:55:47 +0530

    build: Neovim Setup Files

---
+++ nvim/lua/core/colorscheme.lua
@@ -0,0 +1,7 @@
+-- Set colorscheme to 'nightfly' with protected call
+local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+
+if not status then
+  print("Colorscheme not found!") -- print error if colorscheme not installed
+  return
+end
